d9eebb8ce27496dff5a4384067a84d3b95d14ded
Oliver Haack <zccoliver@gmail.com>
2017-09-06 23:27:56 +0200

    first commit-md dialog works

---
+++ src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '@angular/material'; 
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+
+
+import { AppComponent } from './app.component';
+
+import 'hammerjs';
+
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { DishService } from './services/dish.service';
+import { MenuComponent } from './menu/menu.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent,
+    DishdetailComponent,
+    MenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [DishService],
+  bootstrap: [AppComponent],
+  entryComponents: [LoginComponent],
+})
+export class AppModule { }
